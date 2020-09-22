930f6cbeca1921e7a8f55ef4aecaf0c1794c6112
Han Wen Xie <hanw.xie@gmail.com>
2020-09-21 23:20:03 -0400

    Added csv file output

--- HumbleBundleScraper.py
+++ HumbleBundleScraper.py
@@ -1,72 +1,49 @@
 #!/usr/bin/env python
 from bs4 import BeautifulSoup
 import requests
+import csv
+import pandas as pd
 
 url="https://www.humblebundle.com/games/better-futures?hmb_source=navbar&hmb_medium=product_tile&hmb_campaign=tile_index_7"
 req = requests.get(url).text
 
+#open a CSV file to write to
+csv_file = open('HumbleBunder_Scrape.csv', 'w')
+csv_writer = csv.writer(csv_file)
+
 soup = BeautifulSoup(req, 'html.parser')
-#print(soup.prettify().encode("utf-8")
+#print(soup.prettify().encode("utf-8"))
 #print(soup.title)
 
-# Datastructure, empty dictionary
-tiers_dict = {}
 
-#Bundle tiers
-tiers = soup.select(".dd-game-row")
-#print (tiers)
-for tier in tiers:
-    #if has a headline
-    if tier.select(".dd-header-headline"):
-        #Grab tier name and price
-        #print("hello")
-        tiername = tier.select(".dd-header-headline")[0].text.strip()
-        #Grab tier product names
-        product_names = tier.select(".front-page-art-image-text")
-        product_names = [prodname.text.strip() for prodname in product_names]
-
-        # Add
-        tiers_dict[tiername] = {"products": product_names}
-
-for tiername, tierinfo in tiers_dict.items():
-    print(tiername)
-    #print("priced at", tierinfo['price'])
-    print("PRODUCTS:")
-    print("\n".join(tierinfo['products']))
-    print("\n")
-
+#get tiernames and add to csv
 #Bundle tiers class: (price and tier)
-#tier_headlines = soup.select(".dd-header-headline")
+tier_headlines = soup.select(".dd-header-headline")
+#print (tier_headlines)
+stripped_tier_headlines = []
+for tier in tier_headlines:
+    stripped_tier_headlines.append(tier.text.strip())
+    print(tier.text.strip())
+csv_writer.writerow(stripped_tier_headlines)
 
-#stripped_tiernames = []
-#for tier in tier_headlines:
-#    stripped_tiernames.append(tier.text.strip())
-    #print(tier.text.strip())
 
-#Product Names
-#product_names = soup.select(".front-page-art-image-text")
-#stripped_product_names = []
-#for product in product_names:
-#    stripped_product_names.append(product.text.strip())
-    #print(product.text.strip())
+#General tiers
+tiers = soup.select(".dd-game-row")
+#print(tiers)
+all_products = []
+for tier in tiers:
+    #Product Names
+    product_names = tier.select(".front-page-art-image-text")
+    stripped_product_names = []
+    for product in product_names:
+        stripped_product_names.append(product.text.strip())
+    print(stripped_product_names)
+    all_products.append(stripped_product_names)
 
+df = pd.DataFrame(list(zip(*all_products))).add_prefix('Col')
+df.to_csv('file.csv', index=False)
+#csv_writer.writerows(ans)
 #price = [name.split()[1] for name in stripped_tiernames if name.startswith("Pay")]
 #print(price)
 
-#Dictionary datastructure to store info
-#tiers = {
-#    "tier1": {
-#        "price": 500,
-#        "products": [
-#            "name1",
-#            "name2"
-#        ]
-#    },
-#    "tier2": {
-#        "price": 500,
-#        "products": [
-#            "name1",
-#            "name2"
-#        ]
-#    }
-#}
+csv_file.close()
